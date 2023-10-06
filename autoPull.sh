3c39d45d4d1dcb74df08f374bbb7306b49ed588d
Doeca <i@doeca.cc>
2023-10-07 04:35:38 +0800

    [change]:确定项目部署位置

--- autoPull.sh
+++ autoPull.sh
@@ -1,4 +1,5 @@
-﻿cd /home/doeca/documents/projects/clashBack/
+#!/bin/bash
+cd /home/doeca/documents/projects/clashBack/
 export http_proxy=http://127.0.0.1:7890
 export https_proxy=http://127.0.0.1:7890
 cd ./store/
